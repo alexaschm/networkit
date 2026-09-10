7c1e0e30c2c338d4ba2f212ad6afe878d0eef1a1
Alexandra Schmidt <alexandra.schmidt@student.kit.edu>
2026-09-10 16:57:30 +0200

    Add initial python testcases for isomorphism

--- networkit/isomorphism.pyx
+++ networkit/isomorphism.pyx
@@ -14,272 +14,287 @@ from .graph cimport _Graph, Graph
 from .structures cimport count, index, node
 
 cdef extern from "cython_helper.h":
-	void throw_runtime_error(string message)
+	void throw_runtime_error(string message) nogil
 
 cdef cppclass MatchCallbackWrapper:
 	void* callback
+
 	__init__(object callback):
 		this.callback = <void*>callback
+
 	# This is called within the run() method which is nogil!
 	void cython_call_operator(const vector[node]& Match) nogil:
 		cdef bool_t error = False
 		cdef string message
-		# Acquire gil to allow Python code!
+
+		# Acquire GIL to allow Python code.
 		with gil:
 			try:
 				(<object>callback)(Match)
 			except Exception as e:
 				error = True
-				message = stdstring("An Exception occurred, aborting execution of iterator: {0}".format(e))
-			if (error):
-				throw_runtime_error(message)
+				message = stdstring(
+					"An Exception occurred, aborting execution of iterator: {0}".format(e)
+				)
+
+		# Throw only after leaving the with-gil block.
+		if error:
+			throw_runtime_error(message)
+
 
 cdef cppclass ParallelMatchCallbackWrapper:
 	void* callback
+
 	__init__(object callback):
 		this.callback = <void*>callback
+
 	# This is called within the run() method which is nogil!
 	void cython_call_operator(index tid, const vector[node]& Match) nogil:
 		cdef bool_t error = False
 		cdef string message
-		# Acquire gil to allow Python code!
+
+		# Acquire GIL to allow Python code.
 		with gil:
 			try:
 				(<object>callback)(tid, Match)
 			except Exception as e:
 				error = True
-				message = stdstring("An Exception occurred, aborting execution of iterator: {0}".format(e))
-			if (error):
-				throw_runtime_error(message)
+				message = stdstring(
+					"An Exception occurred, aborting execution of iterator: {0}".format(e)
+				)
+
+		# Throw only after leaving the with-gil block.
+		if error:
+			throw_runtime_error(message)
 
 cdef extern from "<networkit/isomorphism/SubgraphIsomorphism.hpp>" namespace "NetworKit":
-    cdef enum class _Semantics "NetworKit::SubgraphIsomorphism::Semantics":
-        INDUCED,
-        MONOMORPHISM
+	cdef enum class _Semantics "NetworKit::SubgraphIsomorphism::Semantics":
+		INDUCED,
+		MONOMORPHISM
 
-    cdef cppclass _SubgraphIsomorphism "NetworKit::SubgraphIsomorphism" (_Algorithm):
-        _SubgraphIsomorphism(const _Graph &pattern, const _Graph &target, _Semantics semantics, count maxMatches) except +
-        count numberOfWorkers() const except +
-        void setNodeLabels(const vector[index] &patternNodeLabels, const vector[index] &targetNodeLabels) except +
-        void setEdgeLabels(const vector[index] &patternEdgeLabels, const vector[index] &targetEdgeLabels) except +
-        void setCallback(MatchCallbackWrapper callback) except +
-        void setCallback(ParallelMatchCallbackWrapper callback) except +
-        void setStoreMatches(bool_t storeMatches) except +
-        const vector[vector[node]] &getMatches() const except +
-        count numberOfMatches() const except +
-        bool_t hasMatch() const except +
+	cdef cppclass _SubgraphIsomorphism "NetworKit::SubgraphIsomorphism" (_Algorithm):
+		_SubgraphIsomorphism(const _Graph &pattern, const _Graph &target, _Semantics semantics, count maxMatches) except +
+		count numberOfWorkers() except +
+		void setNodeLabels(const vector[index] &patternNodeLabels, const vector[index] &targetNodeLabels) except +
+		void setEdgeLabels(const vector[index] &patternEdgeLabels, const vector[index] &targetEdgeLabels) except +
+		void setCallback(MatchCallbackWrapper callback) except +
+		void setCallback(ParallelMatchCallbackWrapper callback) except +
+		void setStoreMatches(bool_t storeMatches) except +
+		const vector[vector[node]] &getMatches() except +
+		count numberOfMatches() except +
+		bool_t hasMatch() except +
 
 class Semantics(object):
-    INDUCED = _Semantics.INDUCED
-    MONOMORPHISM = _Semantics.MONOMORPHISM
+	INDUCED = _Semantics.INDUCED
+	MONOMORPHISM = _Semantics.MONOMORPHISM
 
 cdef class SubgraphIsomorphism(Algorithm):
-    """Abstract base class for subgraph isomorphism algorithms."""
+	"""Abstract base class for subgraph isomorphism algorithms."""
 
-    cdef Graph _pattern
-    cdef Graph _target
-    cdef MatchCallbackWrapper* _callback
-    cdef ParallelMatchCallbackWrapper* _parallelCallback
+	cdef Graph _pattern
+	cdef Graph _target
+	cdef MatchCallbackWrapper* _callback
+	cdef ParallelMatchCallbackWrapper* _parallelCallback
 	cdef object _py_callback
 
-    def __init__(self, *args, **namedargs):
+	def __init__(self, *args, **namedargs):
 		if type(self) == SubgraphIsomorphism:
 			raise RuntimeError("Instantiation of abstract base class")
 
-    def numberOfWorkers(self):
-        """
-        Return the number of workers used by the algorithm.
-        """
-        if self._this == NULL:
+	def numberOfWorkers(self):
+		"""
+		Return the number of workers used by the algorithm.
+		"""
+		if self._this == NULL:
 			raise RuntimeError("Error, object not properly initialized")
-        return (<_SubgraphIsomorphism*>(self._this)).numberOfWorkers()
+		return (<_SubgraphIsomorphism*>(self._this)).numberOfWorkers()
 
-    def setNodeLabels(self, vector[index] patternNodeLabels, vector[index] targetNodeLabels):
-        """
-        setNodeLabels(patternNodeLabels, targetNodeLabels)
+	def setNodeLabels(self, vector[index] patternNodeLabels, vector[index] targetNodeLabels):
+		"""
+		setNodeLabels(patternNodeLabels, targetNodeLabels)
 
-        Set labels for pattern and target nodes. Labels are indexed by node id.
-        """
-        if self._this == NULL:
+		Set labels for pattern and target nodes. Labels are indexed by node id.
+		"""
+		if self._this == NULL:
 			raise RuntimeError("Error, object not properly initialized")
-        return (<_SubgraphIsomorphism*>(self._this)).setNodeLabels(patternNodeLabels, targetNodeLabels)
+		return (<_SubgraphIsomorphism*>(self._this)).setNodeLabels(patternNodeLabels, targetNodeLabels)
 
-    def setEdgeLabels(self, vector[index] patternEdgeLabels,
-                      vector[index] targetEdgeLabels):
-        """
-        setEdgeLabels(patternEdgeLabels, targetEdgeLabels)
+	def setEdgeLabels(self, vector[index] patternEdgeLabels,
+					  vector[index] targetEdgeLabels):
+		"""
+		setEdgeLabels(patternEdgeLabels, targetEdgeLabels)
 
-        Set labels for pattern and target edges. Labels are indexed by edge id.
-        """
-        if self._this == NULL:
+		Set labels for pattern and target edges. Labels are indexed by edge id.
+		"""
+		if self._this == NULL:
 			raise RuntimeError("Error, object not properly initialized")
-        return (<_SubgraphIsomorphism*>(self._this)).setEdgeLabels(patternEdgeLabels, targetEdgeLabels)
+		return (<_SubgraphIsomorphism*>(self._this)).setEdgeLabels(patternEdgeLabels, targetEdgeLabels)
 
-    def setCallback(self, object callback, bool_t parallel=False):
-        """
-        setCallback(callback, parallel=False)
+	def setCallback(self, object callback, bool_t parallel=False):
+		"""
+		setCallback(callback, parallel=False)
 
-        Set a Python callback that is called for every match.
+		Set a Python callback that is called for every match.
 
-        With ``parallel=False``:
-            callback(match)
+		With ``parallel=False``:
+			callback(match)
 
-        With ``parallel=True``:
-            callback(workerId, match)
-        """
-        if self._this == NULL:
-            raise RuntimeError("Error, object not properly initialized")
-
-        if not callable(callback):
-            raise TypeError("callback must be callable")
-
-        # Remove a previously registered wrapper.
-        if self._callback != NULL:
-            del self._callback
-            self._callback = NULL
-
-        if self._parallelCallback != NULL:
-            del self._parallelCallback
-            self._parallelCallback = NULL
-
-        # Keep the Python callback alive.
-        self._py_callback = callback
-
-        if parallel:
-            self._parallelCallback = new ParallelMatchCallbackWrapper(callback)
-            try:
-                (<_SubgraphIsomorphism*>(self._this)).setCallback(
-                    dereference(self._parallelCallback)
-                )
-            except BaseException:
-                del self._parallelCallback
-                self._parallelCallback = NULL
-                self._py_callback = None
-                raise
-        else:
-            self._callback = new MatchCallbackWrapper(callback)
-            try:
-                (<_SubgraphIsomorphism*>(self._this)).setCallback(
-                    dereference(self._callback)
-                )
-            except BaseException:
-                del self._callback
-                self._callback = NULL
-                self._py_callback = None
-                raise
-
-    def __dealloc__(self):
-        if self._callback != NULL:
-            del self._callback
-            self._callback = NULL
-
-        if self._parallelCallback != NULL:
-            del self._parallelCallback
-            self._parallelCallback = NULL
-
-    def setStoreMatches(self, bool_t storeMatches):
-        """
-        setStoreMatches(storeMatches)
-
-        Choose whether found matches are kept for ``getMatches()``.
-        """
-        if self._this == NULL:
+		With ``parallel=True``:
+			callback(workerId, match)
+		"""
+		if self._this == NULL:
 			raise RuntimeError("Error, object not properly initialized")
-        return (<_SubgraphIsomorphism*>(self._this)).setStoreMatches(storeMatches)
 
-    def getMatches(self):
-        """
-        getMatches()
+		if not callable(callback):
+			raise TypeError("callback must be callable")
 
-        Return all stored matches. Each match is indexed by pattern node.
-        """
-        if self._this == NULL:
+		# Remove a previously registered wrapper.
+		if self._callback != NULL:
+			del self._callback
+			self._callback = NULL
+
+		if self._parallelCallback != NULL:
+			del self._parallelCallback
+			self._parallelCallback = NULL
+
+		# Keep the Python callback alive.
+		self._py_callback = callback
+
+		if parallel:
+			self._parallelCallback = new ParallelMatchCallbackWrapper(callback)
+			try:
+				(<_SubgraphIsomorphism*>(self._this)).setCallback(
+					dereference(self._parallelCallback)
+				)
+			except BaseException:
+				del self._parallelCallback
+				self._parallelCallback = NULL
+				self._py_callback = None
+				raise
+		else:
+			self._callback = new MatchCallbackWrapper(callback)
+			try:
+				(<_SubgraphIsomorphism*>(self._this)).setCallback(
+					dereference(self._callback)
+				)
+			except BaseException:
+				del self._callback
+				self._callback = NULL
+				self._py_callback = None
+				raise
+
+	def __dealloc__(self):
+		if self._callback != NULL:
+			del self._callback
+			self._callback = NULL
+
+		if self._parallelCallback != NULL:
+			del self._parallelCallback
+			self._parallelCallback = NULL
+
+	def setStoreMatches(self, bool_t storeMatches):
+		"""
+		setStoreMatches(storeMatches)
+
+		Choose whether found matches are kept for ``getMatches()``.
+		"""
+		if self._this == NULL:
 			raise RuntimeError("Error, object not properly initialized")
-        return (<_SubgraphIsomorphism*>(self._this)).getMatches()
+		return (<_SubgraphIsomorphism*>(self._this)).setStoreMatches(storeMatches)
 
-    def numberOfMatches(self):
-        """
-        Return the number of matches found.
-        """
-        if self._this == NULL:
-			raise RuntimeError("Error, object not properly initialized")
-        return (<_SubgraphIsomorphism*>(self._this)).numberOfMatches()
+	def getMatches(self):
+		"""
+		getMatches()
 
-    def hasMatch(self):
-        """
-        Return whether at least one match was found.
-        """
-        if self._this == NULL:
+		Return all stored matches. Each match is indexed by pattern node.
+		"""
+		if self._this == NULL:
 			raise RuntimeError("Error, object not properly initialized")
-        return (<_SubgraphIsomorphism*>(self._this)).hasMatch()
+		return (<_SubgraphIsomorphism*>(self._this)).getMatches()
+
+	def numberOfMatches(self):
+		"""
+		Return the number of matches found.
+		"""
+		if self._this == NULL:
+			raise RuntimeError("Error, object not properly initialized")
+		return (<_SubgraphIsomorphism*>(self._this)).numberOfMatches()
+
+	def hasMatch(self):
+		"""
+		Return whether at least one match was found.
+		"""
+		if self._this == NULL:
+			raise RuntimeError("Error, object not properly initialized")
+		return (<_SubgraphIsomorphism*>(self._this)).hasMatch()
 
 
 cdef extern from "<networkit/isomorphism/VF2.hpp>" namespace "NetworKit":
-    cdef cppclass _VF2 "NetworKit::VF2" (_SubgraphIsomorphism):
-        _VF2(const _Graph &pattern, const _Graph &target, _Semantics semantics, count maxMatches) except +
+	cdef cppclass _VF2 "NetworKit::VF2" (_SubgraphIsomorphism):
+		_VF2(const _Graph &pattern, const _Graph &target, _Semantics semantics, count maxMatches) except +
 
 cdef class VF2(SubgraphIsomorphism):
-    """
-    VF2(pattern, target, semantics=networkit.isomorphism.Semantics.INDUCED,
-        maxMatches=0)
+	"""
+	VF2(pattern, target, semantics=networkit.isomorphism.Semantics.INDUCED,
+		maxMatches=0)
 
-    Finds every occurrence of a pattern graph inside a target graph using VF2.
-    """
+	Finds every occurrence of a pattern graph inside a target graph using VF2.
+	"""
 
-    cdef Graph _pattern
-    cdef Graph _target
+	cdef Graph _pattern
+	cdef Graph _target
 
-    def __cinit__(self, Graph pattern, Graph target, semantics=_Semantics.INDUCED, maxMatches=0):
-        self._pattern = pattern
-        self._target = target
-        self._this = new _VF2(pattern._this, target._this, semantics, maxMatches)
+	def __cinit__(self, Graph pattern, Graph target, semantics=_Semantics.INDUCED, maxMatches=0):
+		self._pattern = pattern
+		self._target = target
+		self._this = new _VF2(pattern._this, target._this, semantics, maxMatches)
 
 cdef extern from "<networkit/isomorphism/RI.hpp>" namespace "NetworKit":
-    cdef enum class _RIVariant "NetworKit::RI::Variant":
-        RI,
-        RI_DS
+	cdef enum class _RIVariant "NetworKit::RI::Variant":
+		RI,
+		RI_DS
 
-    cdef cppclass _RI "NetworKit::RI" (_SubgraphIsomorphism):
-        _RI(const _Graph &pattern, const _Graph &target, _RIVariant variant, _Semantics semantics, count maxMatches) except +
+	cdef cppclass _RI "NetworKit::RI" (_SubgraphIsomorphism):
+		_RI(const _Graph &pattern, const _Graph &target, _RIVariant variant, _Semantics semantics, count maxMatches) except +
 
 class Variant(object):
-    RI = _RIVariant.RI
-    RI_DS = _RIVariant.RI_DS
+	RI = _RIVariant.RI
+	RI_DS = _RIVariant.RI_DS
 
 cdef class RI(SubgraphIsomorphism):
-    """
-    RI(pattern, target, variant=networkit.isomorphism.Variant.RI,
-       semantics=networkit.isomorphism.Semantics.INDUCED, maxMatches=0)
+	"""
+	RI(pattern, target, variant=networkit.isomorphism.Variant.RI,
+	   semantics=networkit.isomorphism.Semantics.INDUCED, maxMatches=0)
 
-    Finds every occurrence of a pattern graph inside a target graph using RI.
-    """
+	Finds every occurrence of a pattern graph inside a target graph using RI.
+	"""
 
-    cdef Graph _pattern
-    cdef Graph _target
+	cdef Graph _pattern
+	cdef Graph _target
 
-    def __cinit__(self, Graph pattern, Graph target, variant=_RIVariant.RI, semantics=_Semantics.INDUCED, maxMatches=0):
-        self._pattern = pattern
-        self._target = target
-        self._this = new _RI(pattern._this, target._this, variant, semantics, maxMatches)
+	def __cinit__(self, Graph pattern, Graph target, variant=_RIVariant.RI, semantics=_Semantics.INDUCED, maxMatches=0):
+		self._pattern = pattern
+		self._target = target
+		self._this = new _RI(pattern._this, target._this, variant, semantics, maxMatches)
 
 cdef extern from "<networkit/isomorphism/ParallelRI.hpp>":
-    cdef cppclass _ParallelRI "NetworKit::ParallelRI" (_SubgraphIsomorphism):
-        _ParallelRI(const _Graph &pattern, const _Graph &target, _RIVariant variant, _Semantics semantics, count maxMatches) except +
+	cdef cppclass _ParallelRI "NetworKit::ParallelRI" (_SubgraphIsomorphism):
+		_ParallelRI(const _Graph &pattern, const _Graph &target, _RIVariant variant, _Semantics semantics, count maxMatches) except +
 
 cdef class ParallelRI(SubgraphIsomorphism):
-    """
-    ParallelRI(pattern, target, variant=networkit.isomorphism.Variant.RI,
-               semantics=networkit.isomorphism.Semantics.INDUCED, maxMatches=0)
+	"""
+	ParallelRI(pattern, target, variant=networkit.isomorphism.Variant.RI,
+			   semantics=networkit.isomorphism.Semantics.INDUCED, maxMatches=0)
 
-    Parallel version of RI.
-    """
+	Parallel version of RI.
+	"""
 
-    cdef Graph _pattern
-    cdef Graph _target
+	cdef Graph _pattern
+	cdef Graph _target
 
-    def __cinit__(self, Graph pattern, Graph target, variant=_RIVariant.RI, semantics=_Semantics.INDUCED, maxMatches=0):
-        self._pattern = pattern
-        self._target = target
-        self._this = new _ParallelRI(
-            pattern._this, target._this, variant, semantics, maxMatches
-        )
+	def __cinit__(self, Graph pattern, Graph target, variant=_RIVariant.RI, semantics=_Semantics.INDUCED, maxMatches=0):
+		self._pattern = pattern
+		self._target = target
+		self._this = new _ParallelRI(
+			pattern._this, target._this, variant, semantics, maxMatches
+		)
--- networkit/test/test_isomorphism.py
+++ networkit/test/test_isomorphism.py
@@ -1,6 +1,324 @@
 #!/usr/bin/env python3
+
 import unittest
+
 import networkit as nk
-import numpy as np
-import scipy as sc
-import random
+
+
+class TestSubgraphIsomorphism(unittest.TestCase):
+
+    def setUp(self):
+        # Pattern: 0 -- 1
+        self.pattern = nk.Graph(2)
+        self.pattern.addEdge(0, 1)
+
+        # Target: 0 -- 1 -- 2
+        self.target = nk.Graph(3)
+        self.target.addEdge(0, 1)
+        self.target.addEdge(1, 2)
+
+    def testVF2Callback(self):
+        matches = []
+
+        def callback(match):
+            matches.append(match)
+
+        iso = nk.isomorphism.VF2(self.pattern, self.target)
+        iso.setCallback(callback)
+        iso.run()
+
+        self.assertEqual(len(matches), 4)
+        self.assertEqual(len(matches[0]), 2)
+        self.assertEqual(len(matches[1]), 2)
+
+    def testRINumberOfMatches(self):
+        iso = nk.isomorphism.RI(self.pattern, self.target)
+        iso.run()
+
+        self.assertEqual(iso.numberOfMatches(), 4)
+        self.assertTrue(iso.hasMatch())
+
+    def testParallelRICallback(self):
+        matches = []
+
+        def callback(workerId, match):
+            matches.append((workerId, match))
+
+        iso = nk.isomorphism.ParallelRI(self.pattern, self.target)
+        iso.setCallback(callback, parallel=True)
+        iso.run()
+
+        self.assertEqual(len(matches), 4)
+        self.assertEqual(len(matches[0][1]), 2)
+
+    def testCallbackReceivesIndependentMatches(self):
+        pattern = nk.Graph(2)
+        pattern.addEdge(0, 1)
+
+        target = nk.Graph(3)
+        target.addEdge(0, 1)
+        target.addEdge(1, 2)
+
+        matches = []
+
+        def callback(match):
+            matches.append(match)
+
+        iso = nk.isomorphism.VF2(pattern, target)
+        iso.setCallback(callback)
+        iso.run()
+
+        saved = [tuple(match) for match in matches]
+
+        self.assertEqual(len(saved), 4)
+        self.assertEqual(len({id(match) for match in matches}), 4)
+
+    def testCallbackCanMutateReceivedMatch(self):
+        pattern = nk.Graph(2)
+        pattern.addEdge(0, 1)
+
+        target = nk.Graph(3)
+        target.addEdge(0, 1)
+        target.addEdge(1, 2)
+
+        matches = []
+
+        def callback(match):
+            match[0] = 999
+            matches.append(tuple(match))
+
+        iso = nk.isomorphism.VF2(pattern, target)
+        iso.setCallback(callback)
+        iso.run()
+
+        self.assertEqual(len(matches), 4)
+
+    def testCallbackCalledExactlyOncePerMatch(self):
+        pattern = nk.Graph(2)
+        pattern.addEdge(0, 1)
+
+        target = nk.Graph(4)
+        target.addEdge(0, 1)
+        target.addEdge(1, 2)
+        target.addEdge(2, 3)
+
+        callback_count = 0
+
+        def callback(match):
+            nonlocal callback_count
+            callback_count += 1
+
+        iso = nk.isomorphism.VF2(pattern, target)
+        iso.setCallback(callback)
+        iso.run()
+
+        self.assertEqual(callback_count, 6)
+
+    def testCallbackCanBeCallableObject(self):
+        pattern = nk.Graph(2)
+        pattern.addEdge(0, 1)
+
+        target = nk.Graph(3)
+        target.addEdge(0, 1)
+        target.addEdge(1, 2)
+
+        class Callback:
+            def __init__(self):
+                self.matches = []
+
+            def __call__(self, match):
+                self.matches.append(tuple(match))
+
+        callback = Callback()
+
+        iso = nk.isomorphism.VF2(pattern, target)
+        iso.setCallback(callback)
+        iso.run()
+
+        self.assertEqual(len(callback.matches), 4)
+
+    def testCallbackLambda(self):
+        pattern = nk.Graph(2)
+        pattern.addEdge(0, 1)
+
+        target = nk.Graph(2)
+        target.addEdge(0, 1)
+
+        matches = []
+
+        iso = nk.isomorphism.VF2(pattern, target)
+        iso.setCallback(lambda match: matches.append(tuple(match)))
+        iso.run()
+
+        self.assertEqual(len(matches), 2)
+
+    #TODO This test causes an issue that needs to be investigated.
+    def testCallbackRaisesOnFirstMatch(self):
+        pattern = nk.Graph(2)
+        pattern.addEdge(0, 1)
+
+        target = nk.Graph(4)
+        target.addEdge(0, 1)
+        target.addEdge(1, 2)
+        target.addEdge(2, 3)
+
+        calls = 0
+
+        def callback(match):
+            nonlocal calls
+            calls += 1
+            raise RuntimeError("stop")
+
+        iso = nk.isomorphism.RI(pattern, target)
+        iso.setCallback(callback)
+
+        with self.assertRaises(RuntimeError):
+            iso.run()
+
+        self.assertEqual(calls, 1)
+
+    def testDifferentCallbacksDoNotShareState(self):
+        pattern = nk.Graph(2)
+        pattern.addEdge(0, 1)
+
+        target = nk.Graph(3)
+        target.addEdge(0, 1)
+        target.addEdge(1, 2)
+
+        first = []
+        second = []
+
+        def callback1(match):
+            first.append(tuple(match))
+
+        def callback2(match):
+            second.append(tuple(match))
+
+        iso1 = nk.isomorphism.VF2(pattern, target)
+        iso2 = nk.isomorphism.VF2(pattern, target)
+
+        iso1.setCallback(callback1)
+        iso2.setCallback(callback2)
+
+        iso1.run()
+        iso2.run()
+
+        self.assertEqual(len(first), 4)
+        self.assertEqual(len(second), 4)
+        self.assertEqual(first, second)
+
+    def testCallbackKeepsObjectAlive(self):
+        pattern = nk.Graph(2)
+        pattern.addEdge(0, 1)
+
+        target = nk.Graph(3)
+        target.addEdge(0, 1)
+        target.addEdge(1, 2)
+
+        class Callback:
+            def __init__(self):
+                self.count = 0
+
+            def __call__(self, match):
+                self.count += 1
+
+        callback = Callback()
+
+        iso = nk.isomorphism.VF2(pattern, target)
+        iso.setCallback(callback)
+
+        del callback
+        iso.run()
+
+        self.assertEqual(
+            iso.numberOfMatches(),
+            4
+        )
+
+    def testParallelCallbackReceivesValidWorkerIds(self):
+        pattern = nk.Graph(2)
+        pattern.addEdge(0, 1)
+
+        target = nk.Graph(3)
+        target.addEdge(0, 1)
+        target.addEdge(1, 2)
+
+        calls = []
+
+        def callback(workerId, match):
+            calls.append((workerId, tuple(match)))
+
+        iso = nk.isomorphism.ParallelRI(pattern, target)
+        iso.setCallback(callback, parallel=True)
+        iso.run()
+
+        self.assertEqual(len(calls), 4)
+
+        for workerId, match in calls:
+            self.assertGreaterEqual(workerId, 0)
+            self.assertLess(workerId, iso.numberOfWorkers())
+            self.assertEqual(len(match), 2)
+
+    def testParallelCallbackExceptionPropagates(self):
+        pattern = nk.Graph(2)
+        pattern.addEdge(0, 1)
+
+        target = nk.Graph(4)
+        target.addEdge(0, 1)
+        target.addEdge(1, 2)
+        target.addEdge(2, 3)
+
+        calls = []
+
+        def callback(workerId, match):
+            calls.append((workerId, tuple(match)))
+            raise RuntimeError("parallel callback failed")
+
+        iso = nk.isomorphism.ParallelRI(pattern, target)
+        iso.setCallback(callback, parallel=True)
+
+        with self.assertRaises(RuntimeError):
+            iso.run()
+
+        self.assertGreaterEqual(len(calls), 1)
+
+    def testParallelCallbackResultsMatchSerialResults(self):
+        pattern = nk.Graph(3)
+        pattern.addEdge(0, 1)
+        pattern.addEdge(1, 2)
+
+        target = nk.Graph(4)
+        target.addEdge(0, 1)
+        target.addEdge(1, 2)
+        target.addEdge(2, 3)
+
+        serial_matches = []
+        parallel_matches = []
+
+        def serial_callback(match):
+            serial_matches.append(tuple(match))
+
+        def parallel_callback(workerId, match):
+            parallel_matches.append(tuple(match))
+
+        serial = nk.isomorphism.RI(pattern, target)
+        serial.setCallback(serial_callback)
+        serial.run()
+
+        parallel = nk.isomorphism.ParallelRI(pattern, target)
+        parallel.setCallback(parallel_callback, parallel=True)
+        parallel.run()
+
+        self.assertEqual(
+            set(serial_matches),
+            set(parallel_matches)
+        )
+        self.assertEqual(
+            len(serial_matches),
+            len(parallel_matches)
+        )
+    
+
+
+if __name__ == "__main__":
+    unittest.main()
